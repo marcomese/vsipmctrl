d0763a97ab7d622fde2354462dbb340d77ac6720
Marco Mese <mese@na.infn.it>
2021-05-23 11:45:29 +0200

    - Ho rimosso i file vecchi relativi alla Nucleo
    Signed-off-by: Marco Mese <mese@na.infn.it>

--- Debug/Core/Src/fsm.d
+++
@@ -1,3 +0,0 @@
-Core/Src/fsm.o: ../Core/Src/fsm.c ../Core/Inc/fsm.h
-
-../Core/Inc/fsm.h:
--- Debug/Core/Src/fsmParse.d
+++
@@ -1,8 +0,0 @@
-Core/Src/fsmParse.o: ../Core/Src/fsmParse.c ../Core/Inc/fsmParse.h \
- ../Core/Inc/fsm.h ../Core/Inc/configurations.h
-
-../Core/Inc/fsmParse.h:
-
-../Core/Inc/fsm.h:
-
-../Core/Inc/configurations.h:
